4bf3d45622fde8b4e96729c06aca092c6e0e70a7
Oana B <35232908+oannab@users.noreply.github.com>
2021-03-01 19:19:00 +0000

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -1,65 +1,51 @@
 require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const express = require('express')
-const bodyParser = require('body-parser') 
-const fetch = require('node-fetch')
-const path = require('path')
+const app = express();
+const port = 3000;
 
-
-
-const app = express()
-const port = 3000 
-
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
-
-/*
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-  });
-*/
 
 app.use('/', express.static(path.join(__dirname, '../public')))
 
+// your API calls
+app.get("/roverInfo/:rover_name", async (req, res) => {
+  
+  const { rover_name } = req.params
+ // const url = "https://api.nasa.gov/mars-photos/api/v1/";
+  
+  try { //first fetch gets data, second photos
+    const getRover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover_name}?api_key=${process.env.API_KEY}`)
+      .then(res => res.json());
+    const roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?sol=1000&api_key=${process.env.API_KEY}`)
+      .then(res => res.json());
+    res.send(roverPhotos);
+
+  } catch (err) {
+    console.log("errors:", err);
+  }
+})
 
 
-// Picture of the day
+
+// example API call
 app.get('/apod', async (req, res) => {
-  	const { apod } = req.params
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/${apod}?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        	res.send(image)
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
-/*
-// Rover data for each/selected
-app.get('/rover/:rover_name', async (req, res) => {
-  	const { rovers } = req.params
-    try {
-        let rover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${req.params.rover_name}/photos?sol=1000&a?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())           
-            res.send(rover) //send to caller (FE)
+  try {
+    const image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+      .then(res => res.json());
+    res.send({
+      image
+    });
 
-    } catch(err) {
-        console.log('error:', err);
-    }
-})*/
-
-// Get rover's photos by rover's name
-app.get('/rover/:rover_photos', async (req, res) => {
-  	const { rover_photos } = req.params
-    try {
-        let iamges = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover_photos}?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-            res.send(images); 
-    } catch (err) {
-        console.log('error:', err);
-    }
+  } catch (err) {
+    console.log("error:", err);
+  }
 })
 
-
-
-app.listen(port, () => console.log(`Server is running on localhost3000`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
