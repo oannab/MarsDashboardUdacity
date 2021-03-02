a2be4a983396c6e4fb23c35c69f0ace86935821b
Oana B <35232908+oannab@users.noreply.github.com>
2021-03-02 10:43:10 +0000

    Add files via upload

--- src/server/index.js
+++ src/server/index.js
@@ -15,17 +15,34 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 // your API calls
+// example API call
+app.get('/apod', async (req, res) => {
+    try {
+      const image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        .then(res => res.json());
+      res.send({
+        image
+      });
+  
+    } catch (err) {
+      console.log("error:", err);
+    }
+  })
+
+
+  
 app.get("/roverInfo/:rover_name", async (req, res) => {
   
   const { rover_name } = req.params
  // const url = "https://api.nasa.gov/mars-photos/api/v1/";
   
-  try { //first fetch gets data, second photos
-    const getRover = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover_name}?api_key=${process.env.API_KEY}`)
+  try {
+    const rovData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover_name}?api_key=${process.env.API_KEY}`)
       .then(res => res.json());
+      res.send(rovData);
     const roverPhotos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?sol=1000&api_key=${process.env.API_KEY}`)
       .then(res => res.json());
-    res.send(roverPhotos);
+      res.send(roverPhotos);
 
   } catch (err) {
     console.log("errors:", err);
@@ -33,19 +50,5 @@ app.get("/roverInfo/:rover_name", async (req, res) => {
 })
 
 
-
-// example API call
-app.get('/apod', async (req, res) => {
-  try {
-    const image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-      .then(res => res.json());
-    res.send({
-      image
-    });
-
-  } catch (err) {
-    console.log("error:", err);
-  }
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
